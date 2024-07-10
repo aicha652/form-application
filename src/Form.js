3f233bcba3c98fb2db308f4cdc53069707b72986
aicha652 <aichaazlf652@gmail.com>
2024-07-10 21:03:51 +0200

    new Feature

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,11 @@
 import './Form.css'
+import Modal from './Modal'
 import { useState } from "react"
 
 export default function Form() {
+    const [errorMssage, setErrorMessage] = useState(null)
+    const [showModal, setShowModal] = useState(false)
+
     const [form, setForm] = useState({name: "", phone: "", age: "", employee: false, salary: ""})
     function Name(event) {
         return(
@@ -27,8 +31,26 @@ export default function Form() {
 
     const btnIsDisabled = form.name === "" || form.phone === "" || form.age === "" || form.employee === false || form.salary === ""
     
+    function handleFormClick() {
+        if(form.age < 18 || form.age > 100){
+            setErrorMessage("The Age is not Allowed")
+        }
+        else if(form.phone.length < 10 || form.phone.length > 12){
+            setErrorMessage("The phone number format is Incorrect")
+        }
+        else{
+            setErrorMessage(null)
+        }
+        setShowModal(true)
+    }
+
+    function handleDivClass() {
+        if(showModal){
+          setShowModal(false)
+        }
+    }
     return(
-        <div className="divClass">
+        <div onClick={handleDivClass} className="divClass">
             <form 
                onSubmit={(event) => {
                   event.preventDefault()
@@ -56,6 +78,7 @@ export default function Form() {
                     <option>above 2000</option>
                 </select>
                 <button
+                   onClick={handleFormClick}
                    className={btnIsDisabled ? "disable" : ""} 
                    disabled={btnIsDisabled} 
                    id="formButton"
@@ -63,7 +86,7 @@ export default function Form() {
                     Submit
                 </button>
             </form>
-            {/*<Modal />*/}
+            <Modal errorMessage={errorMssage} isVisible={showModal}/>
         </div>
     )
 }
--- src/Modal.js
+++ src/Modal.js
@@ -1,9 +1,14 @@
-export default function Modal() {
-    return(
-        <div id="modal">
-            <div id="modal-content">
-                <h1>The Form Has Been Submitted Successfully</h1>
+export default function Modal({ isVisible, errorMessage=null }) {
+    if (isVisible) {
+        return(
+            <div id="modal">
+                <div id="modal-content"style={{ color: errorMessage != null ? "red" : "green" }}>
+                    <h1>{errorMessage != null ? errorMessage : "The Form Has Been Submitted Successfully"}</h1>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    else {
+        return <></>
+    }
 }
